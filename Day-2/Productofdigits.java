0fb2d7badaff2de687cbec7bd44e27e6050103a1
jainaditya91939-maker <jainaditya1939@gmail.com>
2026-06-05 01:53:10 +0530

    Added Day-2 programs

---
+++ Day-2/Productofdigits.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Productofdigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int product = 1;
+
+        while (num > 0) {
+            int digit = num % 10;
+            product *= digit;
+            num /= 10;
+        }
+
+        System.out.println("Product of digits = " + product);
+    }
+}
