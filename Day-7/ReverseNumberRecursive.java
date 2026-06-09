21aa9efd55b117da57301b38e62a4afb05e5b0ca
jainaditya91939-maker <jainaditya1939@gmail.com>
2026-06-10 01:08:46 +0530

    Added Day-7 java programs

---
+++ Day-7/ReverseNumberRecursive.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class ReverseNumberRecursive {
+    static int reverse(int n, int rev) {
+        if (n == 0)
+            return rev;
+
+        return reverse(n / 10, rev * 10 + n % 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Reverse = " + reverse(n, 0));
+    }
+}
